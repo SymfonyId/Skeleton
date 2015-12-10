821bdc01ee9f55cb555c6f0db35471227c6d1e9a
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2015-12-10 18:39:30 +0700

    add missing User entity

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/*
+ * Author: Muhammad Surya Ihsanuddin<surya.kejawen@gmail.com>
+ * Url: https://github.com/ihsanudin
+ */
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfonian\Indonesia\AdminBundle\Security\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="siab_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
