f41f1d1f76eb9724e56270bc1f9cf39f65a6098f
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2015-12-31 13:12:22 +0700

    update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new Symfonian\Indonesia\CoreBundle\SymfonianIndonesiaCoreBundle(),
             new Symfonian\Indonesia\AdminBundle\SymfonianIndonesiaAdminBundle(),
         );
 
