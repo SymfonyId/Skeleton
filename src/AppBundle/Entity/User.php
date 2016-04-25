778edc71a7bdfc3b62dab4bca9d6067bd1332825
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-04-25 18:20:45 +0700

    update

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,7 +8,7 @@ namespace AppBundle\Entity;
  */
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfonian\Indonesia\AdminBundle\User\User as BaseUser;
+use Symfonian\Indonesia\AdminBundle\Model\User as BaseUser;
 
 /**
  * @ORM\Entity()
