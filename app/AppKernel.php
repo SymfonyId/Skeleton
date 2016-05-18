c571c92297b65c9b974e7dbefbce8eb8e39f6b5a
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-05-18 19:20:06 +0700

    update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,7 +23,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
-            new Symfonian\Indonesia\AdminBundle\SymfonianIndonesiaAdminBundle($this),
+            new Symfonian\Indonesia\AdminBundle\SymfonianIndonesiaAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
